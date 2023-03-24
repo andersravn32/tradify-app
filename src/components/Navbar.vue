11df4f7bb6a7ce36d02248d5ba2904d5e4b072f9
Anders Ravn <anders@adriansen.dk>
2023-03-25 00:50:28 +0100

    Add: Added navigation

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {
+  HomeIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+  ArrowsRightLeftIcon,
+  UserIcon,
+} from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <nav id="navbar">
+    <router-link to="/">
+      <HomeIcon class="h-6 w-6" />
+    </router-link>
+    <router-link to="/">
+      <MagnifyingGlassIcon class="h-6 w-6" />
+    </router-link>
+    <router-link to="/">
+      <PlusIcon class="h-6 w-6" />
+    </router-link>
+    <router-link to="/">
+      <ArrowsRightLeftIcon class="h-6 w-6" />
+    </router-link>
+    <router-link to="/">
+      <UserIcon class="h-6 w-6" />
+    </router-link>
+  </nav>
+</template>
+
+<style>
+#navbar {
+  @apply flex items-center justify-between px-8 border-t-2 border-zinc-800/50 fixed bottom-0 left-0 right-0;
+}
+
+#navbar a {
+  @apply p-4 text-white/50;
+}
+
+#navbar .router-link-active, #navbar .router-link-exact-active{
+    @apply text-white
+}
+</style>
---
+++ src/components/Topnav.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Bars3BottomLeftIcon } from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <header id="topnav">
+    <button class="h-10 w-10">
+      <Bars3BottomLeftIcon class="w-full h-full" />
+    </button>
+    <button class="h-10 w-10 bg-white/10 rounded-full"></button>
+  </header>
+</template>
+
+<style>
+#topnav {
+  @apply flex items-center justify-between p-8 text-white;
+}
+</style>
